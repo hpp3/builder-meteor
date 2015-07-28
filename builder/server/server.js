5962a448744c4611f0a3e165a01e391762ddadee
Eddy Gao <ewgao@uwaterloo.ca>
2015-07-28 15:24:20 -0400

    backup

--- builder/server/server.js
+++ builder/server/server.js
@@ -1,11 +1,18 @@
 Meteor.startup(function () {
     // code to run on server at startup
+    //
+    
     Meteor.methods({
         getKappa: function(){
             return "Kappa";
         },
         getNamedKappa: function(name) {
             return "Kappa = "+name+" (no space)";
+            console.log(getApiKey());
+            console.log('?');
+        },
+        lookupSummoner: function(username, region) {
+            
         }
     });
 });
